519dadac6b7cb04f6883b8849881cdd11c541709
Tanner C <tannerc.bootcamp@gmail.com>
2021-05-08 00:37:21 -0700

    bug fixes and password now generates and writes to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,28 +13,41 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var passwordLength = "7";
+  var passwordLength = 7;
+  password = "";
+  let _quit = false;
   //PasswordLength
-  while (passwordLength < "8" && passwordLength > "128") {
+  while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt(
       "How many characters, ranging from a minimum of 8 to 128, in your password would you like?"
     );
     console.log("Password Length is " + passwordLength);
 
     //Cancel
-    if (passwordLength == null) break;
-
+    if (passwordLength == null) {
+      _quit = true;
+      break;
+    }
     if (!parseInt(passwordLength)) {
-      passwordLength = "7";
+      passwordLength = 7;
       continue;
     }
   }
-  //Character Types
+  if (_quit) return;
+
+  //Character Types Prompts
   let prompts = [
     "Do you want any lowercase letters?",
     "Do you want any capital letters?",
     "Do you want any numeric characters?",
     "Do you want any special characters?",
+  ];
+  //Character Types
+  let charTypes = [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
   ];
 
   let responses = [];
@@ -45,17 +58,21 @@ function generatePassword() {
     input = prompt(prompts[stage], "Yes/No or Y/N");
     switch (input) {
       case null:
+        _quit = true;
         break;
       case "":
         continue;
       default:
         const valid = /(yes|no|y|n)/i.test(input);
         if (!valid) continue;
-        responses[stage] = input;
+        const _yes = /(yes|y)/i.test(input);
+        responses[stage] = _yes;
         stage++;
     }
+    if (_quit) break;
+
     if (stage === 4) {
-      const _check = responses.filter((response) => /(yes|y)/i.test(response));
+      const _check = responses.filter((response) => response);
 
       if (_check.length === 0) {
         alert("You must select at least one character type");
@@ -64,4 +81,18 @@ function generatePassword() {
       }
     }
   }
+  if (_quit) return;
+  //Selecting index into character type array
+  for (i = 0; i < passwordLength; i++) {
+    let _rand = false;
+    while (!responses[_rand]) {
+      _rand = Math.floor(Math.random() * 4);
+    }
+    //Selecting on character from character type at random
+    _typeIndex = Math.floor(Math.random() * charTypes[_rand].length);
+    password += charTypes[_rand].charAt(_typeIndex);
+  }
+  console.log("Password is ${password}");
+
+  return password;
 }
